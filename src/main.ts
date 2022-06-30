75754e975f0e493e0c90a1787cff4cdb0c783c52
Dylan Warrener <dqyqlqaqn@gmail.com>
2022-06-30 21:46:39 +0100

    Added vue router with components

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,37 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { 
+    createRouter, 
+    createWebHistory 
+} from 'vue-router';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+// Components
+import Home from '@/pages/home.vue';
+import Kitchens from '@/pages/kitchens.vue';
+import Bathrooms from '@/pages/Bathrooms.vue';
+import Newbuilds from '@/pages/newbuilds.vue';
+import Extensions from '@/pages/extensions.vue';
+import Refurbishments from '@/pages/refurbishments.vue';
+import Contact from '@/pages/contact.vue';
+import About from '@/pages/about.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: Home },
+        { path: '/kitchens', component: Kitchens },
+        { path: '/bathrooms', component: Bathrooms },
+        { path: '/newbuilds', component: Newbuilds },
+        { path: '/extensions', component: Extensions }, 
+        { path: '/refurbishments', component: Refurbishments }
+        { path: '/contact', component: Contact },
+        { path: '/about', component: About },
+    ]
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
