27957c1d0ef1cd6e24748ad35739bd5820af57da
Dylan Warrener <dqyqlqaqn@gmail.com>
2022-06-30 22:04:38 +0100

    Added navigation

---
+++ src/components/navigation/navigation.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav id="navigation">
+        <router-link to="/"></router-link>
+        <router-link to="/kitchens"></router-link>
+        <router-link to="/bathrooms"></router-link>
+        <router-link to="/newbuilds"></router-link>
+        <router-link to="/extensions"></router-link>
+        <router-link to="/refurbishments"></router-link>
+        <router-link to="/contact"></router-link>
+        <router-link to="/about"></router-link>
+    </nav>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        
+    })
+</script>
+
+<style scoped>
+    #navigation {
+        border: 1px solid red;
+    }
+</style>
