857380ef62ae3d14f255cf9a562e3c7deec7138e
Dylan Warrener <dqyqlqaqn@gmail.com>
2022-06-30 22:04:29 +0100

    Added header

---
+++ src/components/header/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <Navigation />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+// Components
+import Navigation from '@/components/navigation/navigation.vue';
+
+export default defineComponent({
+    components: {
+        Navigation
+    }
+})
+</script>
