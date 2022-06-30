e711f0368ce03bdadbca4dc7a48a988b072cb2d9
Dylan Warrener <dqyqlqaqn@gmail.com>
2022-06-30 22:04:25 +0100

    Added footer

---
+++ src/components/footer/footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer id="footer">
+
+    </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+})
+</script>
+
+<style scoped>
+    #footer {
+        border: 1px solid blue;
+    }
+</style>
