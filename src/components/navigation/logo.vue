2219514af56bd8240366f9d88bc3ebe13c3ab126
Dylan Warrener <dqyqlqaqn@gmail.com>
2022-07-29 12:20:36 +0100

    Added dedicated logo container file

---
+++ src/components/navigation/logo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="container__logo">
+        <img id="logo" src="@/assets/static/svg/logo.svg" alt="logo" />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'logo-component'
+})
+</script>
+
+<style scoped lang="scss">
+    #container__logo {
+        border: 2px solid orange;
+        flex: 1;
+
+        #logo {
+            border: 1px solid red;
+            width: 100%;
+            height: 100%;
+        }
+    }
+</style>
