839845aabde400e35bf4b855087b4d52a7bd9bdd
Dylan Warrener <dqyqlqaqn@gmail.com>
2022-07-29 12:17:33 +0100

    Moved store and routes to their own file

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,18 @@
-// Require imports
+// Required imports
 import { createApp } from 'vue';
-import { 
-    createRouter, 
-    createWebHistory 
-} from 'vue-router';
+
+// Stores
+import store from '@/store';
+
+// Routes
+import router from '@/router';
 
 // Parent component
 import App from './App.vue';
 
-// Child components
-import Home from '@/pages/home.vue';
-import Kitchens from '@/pages/kitchen.vue';
-import Bathrooms from '@/pages/bathroom.vue';
-import Newbuilds from '@/pages/newbuild.vue';
-import Extensions from '@/pages/extension.vue';
-import Refurbishments from '@/pages/refurbishment.vue';
-import Contact from '@/pages/contact.vue';
-import About from '@/pages/about.vue';
+const app = createApp(App);  // Create the app
 
-// Required client routes
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/', component: Home },
-        { path: '/kitchens', component: Kitchens },
-        { path: '/bathrooms', component: Bathrooms },
-        { path: '/newbuilds', component: Newbuilds },
-        { path: '/extensions', component: Extensions }, 
-        { path: '/refurbishments', component: Refurbishments },
-        { path: '/contact', component: Contact },
-        { path: '/about', component: About }
-    ]
-});
+app.use(store);              // Use vuex store
+app.use(router);             // Use vue router
 
-// Create the app
-const app = createApp(App);
-
-// Use vue router
-app.use(router);
-
-// Mount the app
-app.mount('#app');
+app.mount('#app');           // Mount the app
